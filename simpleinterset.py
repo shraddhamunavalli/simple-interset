36a76317f52463a6c3e26320d572c5cfbe3c8d46
shraddha munavalli <munavallishraddha4@gmail.com>
2025-11-03 14:18:04 +0530

    save

---
+++ simpleinterset.py
@@ -0,0 +1,9 @@
+principal = 10000   # Principal amount
+rate = 5            # Rate of interest in percent
+time = 3            # Time in years
+simple_interest = (principal * rate * time) / 100
+print("----- Simple Interest Calculator -----")
+print(f"Principal Amount: ₹{principal}")
+print(f"Rate of Interest: {rate}%")
+print(f"Time Period: {time} years")
+print(f"Calculated Simple Interest: ₹{simple_interest}")
